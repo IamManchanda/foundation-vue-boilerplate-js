0f40f5e98a54ff7cf0f81186e262f80b7ee83386
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-05-09 18:14:11 +0530

    Foundation Setup!

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from '@/App.vue';
 import router from '@/router/index';
 import store from '@/store/index';
 import '@/registerServiceWorker';
+import '@/assets/scss/styles.scss';
 
 Vue.config.productionTip = false;
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,13 @@
+
+const fs = require('fs');
+
 module.exports = {
   lintOnSave: 'error',
+  css: {
+    loaderOptions: {
+      sass: {
+        data: fs.readFileSync('src/assets/scss/abstracts/_settings.scss', 'utf-8'),
+      },
+    },
+  },
 };
